31ba99f898df67e81e09766c4245da265cfc5cd7
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-10-29 21:34:13 +0000

    ten states of nigeria

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
-
+    
 
 class State(BaseModel):
     name: str
@@ -17,24 +17,104 @@ class State(BaseModel):
 
 my_states = [
             {
-                "name": "Lagos",
-                "id": 1,
-                "capital": "Ikeja",
-                "population": 35000000,
-                "nickname": "Centre of Excellence",
-                "governor": "Babajide Sanwo-Olu",
-                "deputy": "Femi Hamzat"
-            },
-            {
-                "name": "Ogun",
-                "id": 2,
-                "capital": "Abeokuta",
-                "population": 6379500,
-                "nickname": "Gateway State",
-                "governor": "Dapo Abiodun",
-                "deputy": "Noimot Salako-Oyedele"
-            }
-            ]
+         "name": "Lagos",
+         "id": 1,
+         "capital": "Ikeja",
+         "population": 35000000,
+         "nickname": "Centre of Excellence",
+         "governor": "Babajide Sanwo-Olu",
+         "deputy": "Femi Hamzat"
+    },
+    {    "name": "Ogun",
+         "id": 2,
+         "capital": "Abeokuta",
+         "population": 6379500,
+         "nickname": "Gateway State",
+         "governor": "Dapo Abiodun",
+         "deputy": "Noimot Salako-Oyedele"
+    },
+    {
+        "name": "Kano",
+        "id": 3,
+        "capital": "Kano",
+        "population": 13740000,
+        "nickname": "Center of Commerce",
+        "governor": "Abdullahi Umar Ganduje",
+        "deputy": "Nasiru Yusuf Gawuna"
+    },
+    {
+        "name": "Kaduna",
+        "id": 4,
+        "capital": "Kaduna",
+        "population": 8272000,
+        "nickname": "Centre of Learning",
+        "governor": "Nasir Ahmad el-Rufai",
+        "deputy": "Hadiza Sabuwa Balarabe"
+    },
+    {
+        "name": "Oyo",
+        "id": 5,
+        "capital": "Ibadan",
+        "population": 7810000,
+        "nickname": "Pace Setter State",
+        "governor": "Seyi Makinde",
+        "deputy": "Rauf Olaniyan"
+    },
+    {
+        "name": "Rivers",
+        "id": 6,
+        "capital": "Port Harcourt",
+        "population": 7395000,
+        "nickname": "Treasure Base of the Nation",
+        "governor": "Nyesom Wike",
+        "deputy": "Ipalibo Banigo"
+    },
+    {
+        "name": "Katsina",
+        "id": 7,
+        "capital": "Katsina",
+        "population": 6611000,
+        "nickname": "Home of Hospitality",
+        "governor": "Aminu Bello Masari",
+        "deputy": "Mannir Yakubu"
+    },
+    {
+        "name": "Bauchi",
+        "id": 8,
+        "capital": "Bauchi",
+        "population": 6537000,
+        "nickname": "Pearl of Tourism",
+        "governor": "Bala Mohammed",
+        "deputy": "Baba Tela"
+    },
+    {
+        "name": "Anambra",
+        "id": 9,
+        "capital": "Awka",
+        "population": 5612000,
+        "nickname": "Light of the Nation",
+        "governor": "Willie Obiano",
+        "deputy": "Nkem Okeke"
+    },
+    {
+        "name": "Jigawa",
+        "id": 10,
+        "capital": "Dutse",
+        "population": 5442000,
+        "nickname": "The New World",
+        "governor": "Muhammad Badaru Abubakar",
+        "deputy": "Umar Namadi"
+    },
+    {
+        "name": "Benue",
+        "id": 11,
+        "capital": "Makurdi",
+        "population": 5316000,
+        "nickname": "Food Basket of the Nation",
+        "governor": "Samuel Ortom",
+        "deputy": "Benson Abounu"
+    }
+    ]
 
 def find_state(id):
     for state in my_states:
